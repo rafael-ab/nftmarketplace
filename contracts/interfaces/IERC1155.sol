96fadbd1b08ed27d2a010ccc620af7348fbe4169
rafius97 <raerocor97@gmail.com>
2021-05-15 08:50:40 -0400

    feat: add erc1155 interface for sending tokens

--- contracts/interfaces/IERC1155.sol
+++ contracts/interfaces/IERC1155.sol
@@ -1,6 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-
-abstract contract imports {}
+interface IERC1155 {
+    function isApprovedForAll(address account, address operator) external view returns (bool);
+    function setApprovalForAll(address operator, bool approved) external;
+    function safeTransferFrom(
+        address from, 
+        address to, 
+        uint256 id, 
+        uint256 amount, 
+        bytes calldata data
+    ) external;
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+}
