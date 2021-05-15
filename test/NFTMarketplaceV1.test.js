55519f9795c4dfea4bbc89c71d7062b8712bc356
rafius97 <raerocor97@gmail.com>
2021-05-15 09:17:42 -0400

    test: add test for accept offer with eth

--- test/NFTMarketplaceV1.test.js
+++ test/NFTMarketplaceV1.test.js
@@ -1,5 +1,5 @@
 const NFTMarketplaceV1 = artifacts.require("NFTMarketplaceV1");
-const ERC1155 = artifacts.require("ERC1155");
+const Token = artifacts.require("Token");
 const { assert, web3 } = require("hardhat");
 const { expectEvent, expectRevert, time } = require("@openzeppelin/test-helpers");
 
@@ -9,7 +9,7 @@ const LINK_ADDRESS = "0x514910771AF9Ca656af840dff83E8264EcF986CA";
 
 const ADMIN = "0xE92d1A43df510F82C66382592a047d288f85226f";
 const SELLER = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
-const BUYER = "0xDf9Eb223bAFBE5c5271415C75aeCD68C21fE3D7F";
+const BUYER = "0x0a4c79cE84202b03e95B7a692E5D728d83C44c76";
 const RECIPIENT = "0x9BF4001d307dFd62B26A2F1307ee0C0307632d59";
 
 const toWei = (value, type) => web3.utils.toWei(String(value), type);
@@ -40,7 +40,7 @@ contract("NFTMarketplaceV1", () => {
       marketplaceV1 = await NFTMarketplaceV1.new({from: ADMIN});
       marketplaceV1.initialize(RECIPIENT, 100, {from: ADMIN});
 
-      // set whitelist for payments
+      // set whitelist tokens for payments
       marketplaceV1.setWhitelistedPaymentToken(WETH_ADDRESS, {from: ADMIN});
       marketplaceV1.setWhitelistedPaymentToken(DAI_ADDRESS, {from: ADMIN});
       marketplaceV1.setWhitelistedPaymentToken(LINK_ADDRESS, {from: ADMIN});
@@ -58,14 +58,15 @@ contract("NFTMarketplaceV1", () => {
 
   it("seller should create an offer", async () => {
     const timestamp = await time.latest();
-    const token = await ERC1155.new("", {from: SELLER});
+
+    const token = await Token.new({from: SELLER});
     await token.setApprovalForAll(marketplaceV1.address, true, {from: SELLER});
-    console.log('await marketplaceV1.offers(SELLER, 1) :>> ', await marketplaceV1.offers(SELLER, 1));
+
     const tx = await marketplaceV1.createOffer(
       token.address,
       1,
       10,
-      timestamp + 360,
+      timestamp + 1,
       250,
       {from: SELLER}
     );
@@ -75,23 +76,24 @@ contract("NFTMarketplaceV1", () => {
       token: token.address,
       tokenId: toBN(1),
       amount: toBN(10),
-      deadline: timestamp + 360,
+      deadline: timestamp + 1,
       priceUSD: toBN(250)
     });
-    console.log('await marketplaceV1.offers(SELLER, 1) :>> ', await marketplaceV1.offers(SELLER, 1));
-    console.log("Gas Used:", tx.receipt.gasUsed);
+    
+    console.log("Gas Used :>> ", tx.receipt.gasUsed);
   });
 
 
   it("seller should cancel an offer", async () => {
     const timestamp = await time.latest();
-    const token = await ERC1155.new("", {from: SELLER});
-    await token.setApprovalForAll(marketplaceV1.address, true, {from: SELLER});
+
+    const token = await Token.new({from: SELLER});
+
     const tx1 = await marketplaceV1.createOffer(
       token.address,
       1,
       10,
-      timestamp + 360,
+      timestamp + 1,
       250,
       {from: SELLER}
     );
@@ -101,10 +103,9 @@ contract("NFTMarketplaceV1", () => {
       token: token.address,
       tokenId: toBN(1),
       amount: toBN(10),
-      deadline: timestamp + 360,
+      deadline: timestamp + 1,
       priceUSD: toBN(250)
     });
-    console.log("Tx1 Gas Used:", tx1.receipt.gasUsed);
 
     const tx2 = await marketplaceV1.cancelOffer(1, {from: SELLER});
 
@@ -113,6 +114,54 @@ contract("NFTMarketplaceV1", () => {
       token: token.address,
       tokenId: toBN(1)
     });
-    console.log("Tx2 Gas Used:", tx2.receipt.gasUsed);
+    console.log("Gas Used :>> ", tx1.receipt.gasUsed + tx2.receipt.gasUsed);
   });
+
+  it("should accept an offer using ETH", async() => {
+    const timestamp = await time.latest();
+
+    const token = await Token.new({from: SELLER});
+    await token.mint(SELLER, 1, 10, 0, {from: SELLER});
+    await token.setApprovalForAll(marketplaceV1.address, true, {from: SELLER});
+
+    console.log('SELLER Token 1 Balance :>> ', String(await token.balanceOf(SELLER, 1)));
+    console.log('BUYER Token 1 Balance :>> ', String(await token.balanceOf(BUYER, 1)));
+    
+    const tx1 = await marketplaceV1.createOffer(
+      token.address,
+      1,
+      10,
+      timestamp + 1,
+      250,
+      {from: SELLER}
+    );
+
+    await expectEvent(tx1, "OfferCreated", {
+      seller: SELLER,
+      token: token.address,
+      tokenId: toBN(1),
+      amount: toBN(10),
+      deadline: timestamp + 1,
+      priceUSD: toBN(250)
+    });
+
+    const tx2 = await marketplaceV1.acceptOfferWithETH(
+      SELLER,
+      1,
+      {from: BUYER, value: toWei(1, "ether")}
+    );
+
+    console.log('SELLER Token 1 Balance :>> ', String(await token.balanceOf(SELLER, 1)));
+    console.log('BUYER Token 1 Balance :>> ', String(await token.balanceOf(BUYER, 1)));
+    
+    console.log("Gas Used :>> ", tx1.receipt.gasUsed + tx2.receipt.gasUsed);
+  })
+
+  it("should accept an offer using DAI", async() => {
+    
+  })
+
+  it("should accept an offer using LINK", async() => {
+    
+  })
 })
