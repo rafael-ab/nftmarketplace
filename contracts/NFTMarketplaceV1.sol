e202f85937bacd1b87064c67914946cc4c93dc7f
rafius97 <raerocor97@gmail.com>
2021-05-16 23:26:05 -0400

    refactor: simplify _getPriceByToken and add mapping of USD/Token price

--- contracts/NFTMarketplaceV1.sol
+++ contracts/NFTMarketplaceV1.sol
@@ -35,6 +35,9 @@ contract NFTMarketplaceV1 is
     // mapping of approved tokens for payment
     mapping(address => bool) private _whitelistedERC20;
 
+    // mapping of token address for chainlink usd-token address
+    mapping(address => address) private _chainlinkUSDToken;
+
     // instance of wrapped eth
     IWETH internal constant weth =
         IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
@@ -367,6 +370,22 @@ contract NFTMarketplaceV1 is
     }
 
     /**
+     * @dev Sets the Chainlink address for the USD pair of the token for payment.
+     *
+     * Requirements:
+     *
+     * - `_tokenPayment` cannot be the zero address.
+     * - `_chainlinkAddress` cannot be the zero address.
+     * - Only the owner can change the address of a `_tokenPayment`.
+     */
+    function setChainlinkUSDToken(
+        address _tokenPayment,
+        address _chainlinkAddress
+    ) external onlyOwner {
+        _chainlinkUSDToken[_tokenPayment] = _chainlinkAddress;
+    }
+
+    /**
      * @dev Sets a whitelist of ERC-20 tokens for payment.
      *
      * Requirements:
@@ -399,33 +418,14 @@ contract NFTMarketplaceV1 is
             _whitelistedERC20[_tokenPayment],
             "NFTMarketplace: TOKEN_NOT_ALLOWED"
         );
+        require(
+            _chainlinkUSDToken[_tokenPayment] != address(0),
+            "NFTMarketplace: TOKEN_ZERO_ADDRESS"
+        );
+        AggregatorV3Interface priceToken =
+            AggregatorV3Interface(_chainlinkUSDToken[_tokenPayment]);
+        (, int256 price, , , ) = priceToken.latestRoundData();
 
-        address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        address link = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
-        uint256 priceUSD;
-        if (_tokenPayment == address(weth)) {
-            AggregatorV3Interface priceETH =
-                AggregatorV3Interface(
-                    0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-                );
-            (, int256 price, , , ) = priceETH.latestRoundData();
-            priceUSD = uint256(price);
-        } else if (_tokenPayment == dai) {
-            AggregatorV3Interface priceDAI =
-                AggregatorV3Interface(
-                    0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9
-                );
-            (, int256 price, , , ) = priceDAI.latestRoundData();
-            priceUSD = uint256(price);
-        } else if (_tokenPayment == link) {
-            AggregatorV3Interface priceLINK =
-                AggregatorV3Interface(
-                    0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c
-                );
-            (, int256 price, , , ) = priceLINK.latestRoundData();
-            priceUSD = uint256(price);
-        }
-
-        return priceUSD;
+        return uint256(price);
     }
 }
